607e1aa3a01f25f76cf4880118cd946ca99d2269
achilep <pahonsibebeto@gmail.com>
2020-07-31 03:04:02 +0100

    TRUNK-5817: Migrate web and webapp package tests to Junit 5

--- web/src/test/java/org/openmrs/web/filter/update/UpdateFilterModelTest.java
+++ web/src/test/java/org/openmrs/web/filter/update/UpdateFilterModelTest.java
@@ -9,46 +9,40 @@
  */
 package org.openmrs.web.filter.update;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openmrs.liquibase.LiquibaseProvider;
-import org.openmrs.util.DatabaseUpdater;
-import org.openmrs.util.DatabaseUpdater.OpenMRSChangeSet;
-import org.openmrs.util.DatabaseUpdaterLiquibaseProvider;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openmrs.liquibase.LiquibaseProvider;
+import org.openmrs.util.DatabaseUpdater;
+import org.openmrs.util.DatabaseUpdater.OpenMRSChangeSet;
+import org.openmrs.util.DatabaseUpdaterLiquibaseProvider;
+import org.powermock.api.mockito.PowerMockito;
+
 /**
  * Tests {@link UpdateFilterModel}.
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(DatabaseUpdater.class)
-@PowerMockIgnore({ "com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "javax.management.*", "org.w3c.dom.*" })
 public class UpdateFilterModelTest {
 	
 	private DatabaseUpdaterLiquibaseProvider liquibaseProvider;
 	
 	private UpdateFilterModel model;
 	
-	@Before
+	@BeforeEach
 	public void setUp() {
 		liquibaseProvider = new DatabaseUpdaterLiquibaseProvider();
 		PowerMockito.mockStatic(DatabaseUpdater.class);
@@ -64,7 +58,7 @@ public class UpdateFilterModelTest {
 		
 		model = new UpdateFilterModel(liquibaseProvider);
 		
-		assertTrue("should require an update", model.updateRequired);
+		assertTrue(model.updateRequired, "should require an update");
 		assertThat(model.changes, is(changes));
 		
 		PowerMockito.verifyStatic(DatabaseUpdater.class);
@@ -84,7 +78,7 @@ public class UpdateFilterModelTest {
 		
 		model = new UpdateFilterModel(liquibaseProvider);
 		
-		assertTrue("should require an update", model.updateRequired);
+		assertTrue(model.updateRequired, "should require an update");
 		assertThat(model.changes, is(empty()));
 		
 		PowerMockito.verifyStatic(DatabaseUpdater.class);
@@ -104,7 +98,7 @@ public class UpdateFilterModelTest {
 		
 		model = new UpdateFilterModel(liquibaseProvider);
 		
-		assertFalse("should not require an update", model.updateRequired);
+		assertFalse(model.updateRequired, "should not require an update");
 		assertThat(model.changes, is(empty()));
 		
 		PowerMockito.verifyStatic(DatabaseUpdater.class);
@@ -123,8 +117,8 @@ public class UpdateFilterModelTest {
 		
 		model = new UpdateFilterModel(liquibaseProvider);
 		
-		assertFalse("should not require an update", model.updateRequired);
-		assertNull("should not have changes", model.changes);
+		assertFalse(model.updateRequired, "should not require an update");
+		assertNull(model.changes, "should not have changes");
 		
 		PowerMockito.verifyStatic(DatabaseUpdater.class);
 		DatabaseUpdater.getUnrunDatabaseChanges(liquibaseProvider);
@@ -142,8 +136,8 @@ public class UpdateFilterModelTest {
 		
 		model = new UpdateFilterModel(liquibaseProvider);
 		
-		assertFalse("should not require an update", model.updateRequired);
-		assertNull("should not have changes", model.changes);
+		assertFalse(model.updateRequired, "should not require an update");
+		assertNull(model.changes, "should not have changes");
 		
 		PowerMockito.verifyStatic(DatabaseUpdater.class, times(2));
 		DatabaseUpdater.getUnrunDatabaseChanges(liquibaseProvider);
@@ -161,7 +155,7 @@ public class UpdateFilterModelTest {
 		
 		model = new UpdateFilterModel(liquibaseProvider);
 		
-		assertTrue("should require an update", model.updateRequired);
+		assertTrue(model.updateRequired, "should require an update");
 		assertThat(model.changes, is(changes));
 		
 		PowerMockito.verifyStatic(DatabaseUpdater.class);
